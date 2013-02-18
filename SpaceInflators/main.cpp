88f1839b955af1f78994001e2b751a2a9868ffa7
David Smallberg <das@david-smallbergs-macbook-pro.local>
2013-02-17 22:52:38 -0800

    Initial Commit

---
+++ SpaceInflators/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SpaceInflators
+//
+//  Created by David Smallberg on 2/17/13.
+//  Copyright (c) 2013 David Smallberg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
